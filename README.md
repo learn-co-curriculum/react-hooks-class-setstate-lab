5b5737665a30d604ecfe9a526976562199bf56c0
ihollander <ianholla@gmail.com>
2020-12-19 11:33:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Class Component State Lab
+
+## Objectives
+
+- Practice refactoring function components to class components
+
+## Instructions
+
+In this lab is the working code from a previous lab, using function components.
+If you run the tests now, they should all pass, except for the tests checking
+that you are using class components instead of functions. Your goal is to
+refactor all the components in the `src/components` directory to use classes
+instead of functions, and _still_ have the tests passing.
+
+Start by familiarizing yourself with the code and the functionality, then work
+through each component and convert them to class components.
+
+Make sure to pay attention to syntax (and in particular, `this` and arrow
+functions). Also make sure you are initializing and setting state correctly.
+
+## Resources
+
+- [State and Lifecycle](https://reactjs.org/docs/state-and-lifecycle.html)
+- [Handling Events](https://reactjs.org/docs/handling-events.html)
---
+++ src/__test__/App.test.js
@@ -0,0 +1,35 @@
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "../components/App";
+import { isClassComponent } from "./utils";
+
+test("uses a class component", () => {
+  expect(isClassComponent(App)).toBe(true);
+});
+
+test("displays in 'light' mode when initialized", () => {
+  const { container } = render(<App />);
+  expect(container.querySelector(".light")).toBeInTheDocument();
+});
+
+test("changes to 'dark' mode when the button is clicked", () => {
+  const { container } = render(<App />);
+  expect(container.querySelector(".light")).toBeInTheDocument();
+
+  fireEvent.click(screen.getByText(/ Mode/));
+
+  expect(container.querySelector(".dark")).toBeInTheDocument();
+});
+
+test("changes back to 'light' mode when the button is clicked twice", () => {
+  const { container } = render(<App />);
+  expect(container.querySelector(".light")).toBeInTheDocument();
+
+  fireEvent.click(screen.getByText(/ Mode/));
+
+  expect(container.querySelector(".dark")).toBeInTheDocument();
+
+  fireEvent.click(screen.getByText(/ Mode/));
+
+  expect(container.querySelector(".light")).toBeInTheDocument();
+});
